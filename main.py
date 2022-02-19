142503d28b471c386e824369043dbd516686a536
Victor Yang <victor.yang@hellosanta.com.tw>
2022-02-19 19:25:29 +0800

    basic usage pillow.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from PIL import Image
 
+image1 = Image.open('teams/ATL.png')
+image1.show()
+image2 = Image.open('teams/BKN.png')
+image2.show()
+image1 = image1.resize((426, 240))
+image1_size = image1.size
+image2_size = image2.size
+new_image = Image.new('RGB',(2*image1_size[0], image1_size[1]), (250,250,250))
+new_image.paste(image1,(0,0))
+new_image.paste(image2,(image1_size[0],0))
+new_image.save("generate/merged_image.jpg","JPEG")
+new_image.show()
