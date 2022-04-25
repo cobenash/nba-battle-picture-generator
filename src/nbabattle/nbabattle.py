2ebfd6d226a3606346483cd716cef22916023d03
cobenash <victor.yang@hellosanta.com.tw>
2022-04-26 00:03:51 +0800

    change background color code4.

--- src/nbabattle/nbabattle.py
+++ src/nbabattle/nbabattle.py
@@ -1,12 +1,11 @@
 import os
 from PIL import Image, ImageDraw
-from colorthief import ColorThief
-
-
 
 class NbaBattle:
     TeamAbb = ['ATL', 'BOS', 'CLE', 'NOP', 'CHI', 'DAL', 'DEN', 'GSW', 'HOU', 'LAC', 'LAL', 'MIA', 'MIL', 'MIN',
                'BKN', 'NYK', 'ORL', 'IND', 'PHI', 'PHX', 'POR', 'SAC', 'SAS', 'OKC', 'TOR', 'UTA', 'MEM', 'WAS', 'DET', 'CHA']
+    TeamColor = {"ATL":(193, 17, 41), "BOS":(0, 119, 63), "CLE":(99, 33, 53), "NOP":(0, 37, 82), "CHI":(204, 21, 51), "DAL":(0, 89, 168), "DEN":(15, 30, 54), "GSW":(253, 176, 35), "HOU":(193, 26, 38), "LAC":(255, 255, 255), "LAL":(70, 37, 115), "MIA":(183, 41, 49), "MIL":(35, 69, 48), "MIN":(0, 37, 82),
+               "BKN":(0, 0, 0), "NYK":(242, 120, 36), "ORL":(5, 107, 180), "IND":(0, 40, 87), "PHI":(233, 22, 67), "PHX":(249, 152, 29), "POR":(175, 17, 35), "SAC":(228, 228, 228), "SAS":(191, 201, 206), "OKC":(12, 115, 186), "TOR":(181, 26, 30), "UTA":(255, 155, 23), "MEM":(114, 144, 185), "WAS":(226, 22, 48), "DET":(219, 27, 47), "CHA":(27, 129,160)}
 
     def __init__(self, teamA, teamB):
         self.teamA = self._is_valid_team(teamA)
@@ -31,8 +30,7 @@ class NbaBattle:
         w_s = int(w/1.3)
         w_h = int(h/1.3)
         team_logo = team_logo.resize((w_s, w_h), Image.ANTIALIAS)
-        color_thief = ColorThief(logo_path)
-        color_code =  color_thief.get_color(quality=1)
+        color_code = self.TeamColor[team]
         background_image = Image.new("RGB", (285, 400), color_code)
         team_logo = team_logo.convert("RGBA")
         background_image = background_image.convert("RGBA")
